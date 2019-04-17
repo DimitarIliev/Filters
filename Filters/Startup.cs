93e989f5001d0f5948d462fe1923a3155f619d91
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-04-17 23:12:32 +0200

    Finished Filter chapter examples.

--- Filters/Startup.cs
+++ Filters/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Filters.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,20 +16,28 @@ namespace Filters
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSingleton<IFilterDiagnostics, DefaultFilterDiagnostics>();
+            //services.AddSingleton<TimeFilter>();
+            //services.AddMvc();
+            services.AddScoped<IFilterDiagnostics, DefaultFilterDiagnostics>();
+            services.AddScoped<TimeFilter>();
+            services.AddScoped<ViewResultDiagnostics>();
+            services.AddScoped<DiagnosticsFilter>();
+            services.AddMvc().AddMvcOptions(options =>
+            {
+                options.Filters.Add(new MessageAttribute("This is the Globally-Scoped Filter"));
+                //options.Filters.AddService(typeof(ViewResultDiagnostics));
+                //options.Filters.AddService(typeof(DiagnosticsFilter));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
